0c1bcba631d3aea35939384a626e2c0f37b19231
Shubham Rawat <shubhamrawat1808@gmail.com>
2025-08-05 21:33:43 +0530

    fake commit

--- bank-management/src/main/java/com/bankManagement/bank_management/BankManagementApplication.java
+++ bank-management/src/main/java/com/bankManagement/bank_management/BankManagementApplication.java
@@ -8,5 +8,6 @@ public class BankManagementApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BankManagementApplication.class, args);
         System.out.println("Hello!World");
+
 	}
 }
